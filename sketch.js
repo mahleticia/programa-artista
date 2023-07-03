0f02032c3b5d1527391a595c338a9920a1e7225e
mahleticia <137805694+mahleticia@users.noreply.github.com>
2023-07-03 12:03:37 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(800, 800);
+  background(230);
+}
+
+function draw() {
+  stroke("blue");
+  fill("red");
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 2, 3);
+  }
+}
